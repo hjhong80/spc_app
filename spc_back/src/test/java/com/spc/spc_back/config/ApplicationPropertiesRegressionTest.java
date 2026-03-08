6141e01dbc3bd276bf902fcda4aebb8ea15b0ccd
hjhong80 <ghdgowns@gmail.com>
2026-03-08 22:48:41 +0900

    chore : swagger-toggle shell script

--- spc_back/src/test/java/com/spc/spc_back/config/ApplicationPropertiesRegressionTest.java
+++ spc_back/src/test/java/com/spc/spc_back/config/ApplicationPropertiesRegressionTest.java
@@ -14,12 +14,28 @@ class ApplicationPropertiesRegressionTest {
 
     @Test
     void 운영설정은프록시전달헤더를신뢰한다() throws IOException {
+        Properties properties = loadProperties(Path.of("src", "main", "resources", "application.properties"));
+
+        assertThat(properties.getProperty("server.forward-headers-strategy")).isEqualTo("framework");
+    }
+
+    @Test
+    void 운영설정은스웨거토글환경변수를노출한다() throws IOException {
+        Properties properties = loadProperties(Path.of("src", "main", "resources", "application.properties"));
+
+        assertThat(properties.getProperty("springdoc.api-docs.enabled"))
+                .isEqualTo("${SPC_SPRINGDOC_API_DOCS_ENABLED:true}");
+        assertThat(properties.getProperty("springdoc.swagger-ui.enabled"))
+                .isEqualTo("${SPC_SPRINGDOC_SWAGGER_UI_ENABLED:true}");
+    }
+
+    private Properties loadProperties(Path path) throws IOException {
         Properties properties = new Properties();
 
-        try (InputStream inputStream = Files.newInputStream(Path.of("src", "main", "resources", "application.properties"))) {
+        try (InputStream inputStream = Files.newInputStream(path)) {
             properties.load(inputStream);
         }
 
-        assertThat(properties.getProperty("server.forward-headers-strategy")).isEqualTo("framework");
+        return properties;
     }
 }
